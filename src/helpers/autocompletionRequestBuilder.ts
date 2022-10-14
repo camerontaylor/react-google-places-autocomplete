e6022835c7a5d649be220e5aa532bec2c6836c4d
Cameron Taylor <camerontaylor@gmail.com>
2022-10-14 13:42:26 +1100

    fix array use

--- src/helpers/autocompletionRequestBuilder.ts
+++ src/helpers/autocompletionRequestBuilder.ts
@@ -17,7 +17,7 @@ export default (
   }
 
   if (bounds) {
-    res.bounds = Array.isArray(bounds)  ? new google.maps.LatLngBounds(...bounds) : new google.maps.LatLngBounds(bounds);
+    res.bounds = Array.isArray(bounds)  ? new google.maps.LatLngBounds(bounds[0], bounds[1]) : bounds ;
   }
 
   if (location) {
